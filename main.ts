f70661e9d93a0c53010bdc9c3b9cc21c9ddcdff2
aimakerhk <59435476+STEM-AIMAKER@users.noreply.github.com>
2022-05-19 14:00:59 +0800

    Update main.ts
    Add  air conditioner function

--- main.ts
+++ main.ts
@@ -7,6 +7,44 @@ namespace HANSHIN_IAQ_IOT {
  */
 // weight=10 color=#1E90FF icon="\uf136" block="HANSHIN: IAQ & IOT"
 
+	enum TemperatureValue {
+	  //% block=16
+	  T16 = 16,
+	  //% block=17
+	  T17 = 17,
+	  //% block=18
+	  T18 = 18,
+	  //% block=19
+	  T19 = 19,
+	  //% block=20
+	  T20 = 20,
+	  //% block=21
+	  T21 = 21,
+	  //% block=22
+	  T22 = 22,
+	  //% block=23
+	  T23 = 23,
+	  //% block=24
+	  T24 = 24,
+	  //% block=25
+	  T25 = 25,
+	  //% block=26
+	  T26 = 26,
+	  //% block=27
+	  T27 = 27,
+	  //% block=28
+	  T28 = 28,
+	  //% block=29
+	  T29 = 29,
+	  //% block=30
+	  T30 = 30,
+	  //% block=31
+	  T31 = 31,
+	  //% block=32
+	  T26 = 32
+	};
+
+
        export enum CurtainIndex {
         //% blockId="First" block="First"
         First = 0,
@@ -53,9 +91,45 @@ namespace HANSHIN_IAQ_IOT {
         basic.pause(200)
         serial.writeString("+++")
     }
-        
+	
+	//% blockId=changeAirConditionerTemperature block="Change air conditioner |index=%index to |temperature=%temperature"
+	export function changeAirConditionerTemperature(index: number, temperature: TemperatureValue ): void {
+		serial.writeString("AT+CIPMODE=1") 
+        basic.pause(200)
+        serial.writeString("AT+CIPSEND")
+        basic.pause(200)
+        let ioCmd = "at," + (int)temperature + "," + index
+        serial.writeString(ioCmd)
+        basic.pause(200)
+        serial.writeString("+++")
+	}
+    
+	//% blockId=turnOffAirConditioner block="Turn off air conditioner , |index=%index"
+	export function turnOffAirConditioner(index: number): void {
+	    serial.writeString("AT+CIPMODE=1") 
+        basic.pause(200)
+        serial.writeString("AT+CIPSEND")
+        basic.pause(200)
+        let ioCmd = "ap,0," + index
+        serial.writeString(ioCmd)
+        basic.pause(200)
+        serial.writeString("+++")
+	}
+    
+	//% blockId=turnOnAirConditioner block="Turn on air conditioner , |index=%index"
+	export function turnOnAirConditioner(index: number): void {
+	    serial.writeString("AT+CIPMODE=1") 
+        basic.pause(200)
+        serial.writeString("AT+CIPSEND")
+        basic.pause(200)
+        let ioCmd = "ap,1," + index
+        serial.writeString(ioCmd)
+        basic.pause(200)
+        serial.writeString("+++")
+	}
+	
     //% blockId=turnOffIO block="Turn off io, |io Index=%ioIndex"
-    export function turnOffIO(ioIndex: string): void {
+    export function turnOffIO(ioIndex: number): void {
         serial.writeString("AT+CIPMODE=1") 
         basic.pause(200)
         serial.writeString("AT+CIPSEND")
@@ -67,7 +141,7 @@ namespace HANSHIN_IAQ_IOT {
     }
     
     //% blockId=turnOnIO block="Turn on io, |io Index=%ioIndex"
-    export function turnOnIO(ioIndex: string): void {
+    export function turnOnIO(ioIndex: number): void {
         serial.writeString("AT+CIPMODE=1")
         basic.pause(200)
         serial.writeString("AT+CIPSEND")
